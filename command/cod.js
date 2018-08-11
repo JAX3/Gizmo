8301e03585398e5a3dc534ff7148888343831f45
JAX3 <thecore95x@gmail.com>
2018-08-11 23:05:47 +0100

    Delete cod.js

--- command/cod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'cod',
-    description: 'Gives the user the Overwatch role',
-    execute(message) {
-        let roleid = message.member.guild.roles.find("name", "Call of Duty");
-        message.member.addRole(roleid);
-        message.reply(`Has been given a Role.`)
-    },
-  };
