9f14ce0ef0bce85ebb4aa4099eeac25fcdb9bf04
JAX3 <thecore95x@gmail.com>
2018-08-11 23:07:54 +0100

    Delete pubg.js

--- command/pubg.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'pubg',
-    description: 'Gives the user the Hearthstone role',
-    execute(message) {
-        let roleid = message.member.guild.roles.find("name", "PUBG");
-        message.member.addRole(roleid);
-        message.reply(`Has been given a Role.`)
-    },
-  };
