d9149d58b836c74802c04d53c38aa2a0a0dbabf1
JAX3 <thecore95x@gmail.com>
2018-08-11 23:08:01 +0100

    Delete restart.js

--- command/restart.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'restart',
-    description: 'restarts the bot ',
-    execute(message) {
-        process.exit(1);
-        
-        message.delete().catch(O_o=>{});
-    },
-  };
