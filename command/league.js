858dc821adf9c7947bbe9d77ee74ac6838a7e75e
JAX3 <thecore95x@gmail.com>
2018-08-11 23:07:08 +0100

    Delete league.js

--- command/league.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'league',
-    description: 'Gives the user the Hearthstone role',
-    execute(message) {
-        let roleid = message.member.guild.roles.find("name", "League of Legends");
-        message.member.addRole(roleid);
-        message.reply(`Has been given a Role.`)
-    },
-  };
