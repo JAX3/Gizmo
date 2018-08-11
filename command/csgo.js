947304ea7630b16ad3ef450879fc394aa0da06b1
JAX3 <thecore95x@gmail.com>
2018-08-11 23:05:52 +0100

    Delete csgo.js

--- command/csgo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'csgo',
-    description: 'Gives the user the csgo role',
-    execute(message) {
-        let roleid = message.member.guild.roles.find("name", "CS:GO");
-        message.member.addRole(roleid);
-        message.reply(`Has been given a Role.`)
-    },
-  };
