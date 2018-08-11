d9d64c3ed412c0d41b3fb4eb2d308171e23dd7fd
JAX3 <thecore95x@gmail.com>
2018-08-11 23:07:41 +0100

    Delete rainbow6.js

--- command/rainbow6.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'r6',
-    description: 'Gives the user the Overwatch role',
-    execute(message) {
-        let roleid = message.member.guild.roles.find("name", "Rainbow Six Siege");
-        message.member.addRole(roleid);
-        message.reply(`Has been given a Role.`)
-    },
-  };
